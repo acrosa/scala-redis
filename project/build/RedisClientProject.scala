1240efe93f9e6c7718536842216e5a50564f4587
Alejandro Crosa <alejandro.crosa@gmail.com>
2010-05-23 20:45:36 -0700

    removed unneeded deps new version for sbt

--- project/build/RedisClientProject.scala
+++ project/build/RedisClientProject.scala
@@ -4,9 +4,7 @@ class RedisClientProject(info: ProjectInfo) extends DefaultProject(info) with Au
 {
   override def useDefaultConfigurations = true
 
-  val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
   val specs = "org.scala-tools.testing" % "specs" % "1.5.0"
   val mockito = "org.mockito" % "mockito-all" % "1.7"
   val junit = "junit" % "junit" % "4.5"
-  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.1")
 }
