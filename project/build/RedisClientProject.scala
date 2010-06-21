d04532102c51d7820bf25bc357cf11d9c579176d
kevinp <kevinp@kevinp.(none)>
2010-06-21 14:09:14 -0700

    Update specs and mockito versions to support scala 2.8

--- project/build/RedisClientProject.scala
+++ project/build/RedisClientProject.scala
@@ -4,7 +4,7 @@ class RedisClientProject(info: ProjectInfo) extends DefaultProject(info) with Au
 {
   override def useDefaultConfigurations = true
 
-  val specs = "org.scala-tools.testing" % "specs" % "1.5.0"
-  val mockito = "org.mockito" % "mockito-all" % "1.7"
+  val specs = "org.scala-tools.testing" % "specs_2.8.0.Beta1" % "1.6.2"
+  val mockito = "org.mockito" % "mockito-all" % "1.8.1"
   val junit = "junit" % "junit" % "4.5"
 }
